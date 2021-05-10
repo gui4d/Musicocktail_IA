8946f363e8021cf8e4081ec6d3541b05a17e22b9
gui4d <38852724+gui4d@users.noreply.github.com>
2021-05-10 15:02:01 +0200

    initial commit

---
+++ Music_Analysis.py
@@ -0,0 +1,16 @@
+from Music import *
+
+
+
+class Musi_Analysis():
+
+    def __init__(self, Music):
+        self.Music = Music
+    
+    def extract_Data(): 
+        return
+
+    def extract_extractors(): 
+        return
+    
+    
